b0c6930772577c88799e14f888698818dc77e79a
ural1493 <ural1493@gmail.com>
2021-02-21 17:43:06 +0300

    feat: add function code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (Array.isArray(array) && array.length) {
+        return array.sort((a,b)=>a-b)[0];
+    }
+    else return 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (Array.isArray(array) && array.length) {
+        return array.sort((a,b)=>b-a)[0];
+    }
+    else return 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (Array.isArray(array) && array.length) {
+        return array.reduce((calc, value, index) => (calc+value))/array.length;
+    }
+    else return 0;
 }
